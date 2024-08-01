9eb340faac11152099528eae9213be0a633fb0c8
bava-kurian <bavakurian135@gmail.com>
2024-08-01 20:07:54 +0530

    Create two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        Map = {}  # val -> index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in Map:
+                return [Map[diff], i]
+            Map[n] = i
