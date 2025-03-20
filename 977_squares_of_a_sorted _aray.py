016509565e90a8719b198a87ad8207845adeff9d
bava-kurian <bavakurian135@gmail.com>
2025-03-20 23:03:49 +0530

    Add alternative solution for 'Squares of a Sorted Array' problem

--- 977_squares_of_a_sorted _aray.py
+++ 977_squares_of_a_sorted _aray.py
@@ -4,3 +4,37 @@ class Solution:
         for n in nums:
             s.append(n**2)
         return sorted(s)
+    
+    
+    ### another solution
+    # class Solution:
+    # def sortedSquares(self, nums: List[int]) -> List[int]:
+    #     s=[]
+    #     m=0
+    #     if nums[0] >= 0: # all positive
+    #         s=[x**2 for x in nums]
+    #         return s
+    #     if len(nums)==1: # single number
+    #         return [nums[0]**2]
+    #     for i,n in enumerate(nums): #find +ve negative boundary
+    #         if n>=0:
+    #             m=i
+    #             break
+    #     if m==0: # all negative
+    #         return  [x**2 for x in nums] [::-1]
+
+    #     a=[x**2 for x in nums[m::]]
+    #     b=[x**2 for x in nums[m-1::-1]]
+
+    #     while(a and b): # merge
+    #         if a[0]<=b[0]:
+    #             s.append(a.pop(0))
+                
+    #         else :
+    #             s.append(b.pop(0))
+    #     if a :
+    #         s+=[x for x in a]
+    #     if b :
+    #         s+=[x for x in b]
+    #     return s
+
