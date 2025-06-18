6f30610c8c01d72f9da59491d4f15a366d90f35f
bava-kurian <bavakurian135@gmail.com>
2025-06-18 13:04:06 +0530

    Remove commented-out code for 'permuteUnique' method to improve clarity

--- 46_permutations.py
+++ 46_permutations.py
@@ -13,4 +13,21 @@ class Solution:
         helper(0)
         return result
             
-        
+        #or we can use hashmap to store the visited elements are improve the complexity 
+    # class Solution:
+    # def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+    #     l=len(nums)
+    #     result=[]
+    #     def helper(index):
+    #         if index==l-1:
+    #             result.append(nums[:])
+    #             return 
+    #         hash={}
+    #         for j in range(index,l):
+    #             if nums[j] not in hash:
+    #                 hash[nums[j]]=True
+    #                 nums[index],nums[j]=nums[j],nums[index]
+    #                 helper(index+1)
+    #                 nums[j],nums[index]=nums[index],nums[j]
+    #     helper(0)
+    #     return result
