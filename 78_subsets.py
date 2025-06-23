3464306180a238a4e4cb56a7fb82bd16c9ab9824
bava-kurian <bavakurian135@gmail.com>
2025-06-23 13:57:59 +0530

    Add memoization implementation for Fibonacci function

--- 78_subsets.py
+++ 78_subsets.py
@@ -11,3 +11,20 @@ class Solution:
             subset.pop()
         helper(nums,0,[])
         return output
+    
+    # memoization
+
+    def fib(self, n: int) -> int:
+        hash={}
+        def f(n):
+            if n in hash:
+                return hash[n]
+            if n<=1:
+                return n
+            hash[n]=self.fib(n-1)+self.fib(n-2)
+            return hash[n]
+        return f(n)
+    
+    
+#Tabulation
+
