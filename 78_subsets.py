4bc12305a1407773212d63fe59cde776f23c5699
bava-kurian <bavakurian135@gmail.com>
2025-06-23 14:16:10 +0530

    Implement tabulation method for Fibonacci sequence calculation

--- 78_subsets.py
+++ 78_subsets.py
@@ -27,4 +27,15 @@ class Solution:
     
     
 #Tabulation
+class Solution:
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
 
+        dp = [0] * (n + 1)  # allocate enough space for dp[0] through dp[n]
+        dp[0], dp[1] = 0, 1
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
