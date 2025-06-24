49ba063f5580a48416dd5a98aa1b21ff82c09fe8
bava-kurian <bavakurian135@gmail.com>
2025-06-24 23:07:18 +0530

    Implement climbing stairs solution with space optimization

---
+++ 70_climbing_stairs.py
@@ -0,0 +1,25 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
+#space optimization
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        first, second = 1, 2
+
+        for i in range(3, n + 1):
+            first, second = second, first + second
+
+        return second
