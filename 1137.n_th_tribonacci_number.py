8049d8675c95b562e05bb7b752094eaccefa49d1
bava-kurian <bavakurian135@gmail.com>
2025-06-25 13:37:27 +0530

    Implement space optimization method for n-th Tribonacci number calculation

--- 1137.n_th_tribonacci_number.py
+++ 1137.n_th_tribonacci_number.py
@@ -25,3 +25,15 @@ class Solution:
                 dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
         return dp[n]
             
+#space optimization
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+
+        a, b, c = 0, 1, 1
+        for _ in range(3, n + 1):
+            a, b, c = b, c, a + b + c
+        return c
