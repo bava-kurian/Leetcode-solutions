872063092d9a8d3ada73d478b45efab598bb963e
bava-kurian <bavakurian135@gmail.com>
2025-06-25 13:36:07 +0530

    Implement tabulation method for n-th Tribonacci number calculation

--- 1137.n_th_tribonacci_number.py
+++ 1137.n_th_tribonacci_number.py
@@ -11,3 +11,17 @@ class Solution:
                 memo[index]=helper(index-3)+helper(index-2)+helper(index-1)
             return memo[index]
         return helper(n)
+
+#tabulation
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n==0:
+            return 0
+        elif n==1 or n==2:
+            return 1
+        dp=[0]*(n+2)
+        dp[0], dp[1], dp[2] = 0, 1, 1
+        for i in range(3,n+1):
+                dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
+        return dp[n]
+            
