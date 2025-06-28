d7073752a77b6ba8cbf8bb3e8d95fc633fdd9b53
bava-kurian <bavakurian135@gmail.com>
2025-06-28 21:11:16 +0530

    Refactor target sum problem solution to enhance clarity and maintainability with memoization techniques

--- 494_target_sum.py
+++ 494_target_sum.py
@@ -1,6 +1,6 @@
-#recursive solution to target sum problem
+#recursive solution using memoization to find the number of ways to achieve a target sum with given numbers using lru_cache
 from functools import lru_cache
-
+from typing import List
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         n=len(nums)
@@ -9,4 +9,17 @@ class Solution:
             if index==n:
                 return 0 if sum!=target else 1
             return helper(index+1,sum+nums[index]) +helper(index+1,sum-nums[index])
+        return helper(0,0)
+    
+#susing memoization to find the number of ways to achieve a target sum with given numbers using dictionary
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        memo={}
+        def helper(index,sum):
+            if index==n:
+                return 0 if sum!=target else 1
+            if (index,sum) not in memo:
+                memo[(index,sum)]=helper(index+1,sum+nums[index]) +helper(index+1,sum-nums[index])
+            return memo[index,sum]
         return helper(0,0)
