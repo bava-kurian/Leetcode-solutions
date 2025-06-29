53877d5e480fb031fb2082257e2bee02a528f706
bava-kurian <bavakurian135@gmail.com>
2025-06-29 20:26:03 +0530

    Implement memoization in canPartition method to optimize subset sum partitioning

--- 416_partition_equal_subset_sum.py
+++ 416_partition_equal_subset_sum.py
@@ -17,4 +17,17 @@ class Solution:
             
 
 
-            
+ # Using memoization to check if we can partition the array into two subsets with equal sum
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        n=len(nums)
+        if sum(nums) %2!=0: return False
+        target=sum(nums)//2
+        @lru_cache(None)
+        def helper(i,curr_summ):
+            if target==curr_summ:
+                return True
+            if i>=n or curr_summ>target: 
+                return False
+            return helper(i+1,curr_summ+nums[i]) or helper(i+1,curr_summ)
+        return helper(0,0)
