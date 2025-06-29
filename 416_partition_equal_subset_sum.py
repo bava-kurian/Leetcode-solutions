b783ae8b7d36386be78c91718b2147e3a29ec936
bava-kurian <bavakurian135@gmail.com>
2025-06-29 20:26:20 +0530

    Add tabulation approach for partition equal subset sum problem

--- 416_partition_equal_subset_sum.py
+++ 416_partition_equal_subset_sum.py
@@ -31,3 +31,24 @@ class Solution:
                 return False
             return helper(i+1,curr_summ+nums[i]) or helper(i+1,curr_summ)
         return helper(0,0)
+    
+    
+    #usinsg tabulation to check if we can partition the array into two subsets with equal sum
+    class Solution:
+        def canPartition(self, nums: List[int]) -> bool:
+            n=len(nums)
+            if sum(nums) %2!=0: return False
+            target=sum(nums)//2
+            dp=[[False]*(target+1) for _ in range(n+1)]
+            for i in range(n + 1):
+                dp[i][0] = True
+
+            for i in range(1, n + 1):
+                for s in range(1, target + 1):
+                    if s >= nums[i - 1]:
+                        dp[i][s] = dp[i - 1][s] or dp[i - 1][s - nums[i - 1]]
+                    else:
+                        dp[i][s] = dp[i - 1][s]
+
+            return dp[n][target]
+                
