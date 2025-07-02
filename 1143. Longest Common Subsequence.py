eec6a1b2f2638f5d36e2a5a674af7efc20ebc180
bava-kurian <bavakurian135@gmail.com>
2025-07-02 23:02:59 +0530

    Implement recursive solution for longest common subsequence problem

---
+++ 1143. Longest Common Subsequence.py
@@ -0,0 +1,13 @@
+
+#recursion
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n=len(text1)
+        m=len(text2)
+        def helper(i,j):
+            if i>(n-1) or j>(m-1):
+                return 0
+            if text1[i]==text2[j]:
+                return 1+helper(i+1,j+1)
+            return max(helper(i+1,j),helper(i,j+1))
+        return helper(0,0)
