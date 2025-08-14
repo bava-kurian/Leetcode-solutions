d0ed9c60e5fc8fdad6c97dae53e8f20f6e7beb6d
bava-kurian <bavakurian135@gmail.com>
2025-08-14 22:43:37 +0530

    152. Maximum Product Subarray

--- 152. Maximum Product Subarray.py
+++ 152. Maximum Product Subarray.py
@@ -16,3 +16,19 @@ class Solution:
             return max_here, min_here, global_max
 
         return helper(len(nums) - 1)[2]
+#tabular solution
+def maxProduct(nums):
+    max_prod = nums[0]
+    min_prod = nums[0]
+    result = nums[0]
+
+    for i in range(1, len(nums)):
+        if nums[i] < 0:
+            max_prod, min_prod = min_prod, max_prod  # swap when negative
+
+        max_prod = max(nums[i], nums[i] * max_prod)
+        min_prod = min(nums[i], nums[i] * min_prod)
+
+        result = max(result, max_prod)
+
+    return result
