ac2f470049a2cada3bb26a706688b316cbe66f0e
bava-kurian <bavakurian135@gmail.com>
2025-08-18 11:20:05 +0530

    62. Unique Paths

---
+++ 62. Unique Paths.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp=[[0]*n for _ in range(m)]
+        print(dp)
+        for i in range(m):
+            dp[i][0]=1
+        for j in range(n):
+            dp[0][j]=1            
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j]=dp[i][j-1]+dp[i-1][j]
+        return dp[m-1][n-1]
